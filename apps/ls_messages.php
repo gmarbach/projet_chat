100e81d9dd4818980d5fb0a2cb7b63d5b70e42e1
GaelBy <g.benay@hotmail.fr>
2016-06-10 14:42:48 +0200

    correction message

--- apps/ls_messages.php
+++ apps/ls_messages.php
@@ -1,3 +1,12 @@
 <?php
+$manager = new MessageManager($link);
+$message_list = $manager->getAll();
+$no_message = 'hide';
+$messages = '';
+if (empty($message_list))
+{
+	$no_message = '';
+	$messages = 'hide';
+}
 require('views/ls_messages.phtml');
 ?>
