56e6f5c7c5c3122de1ea44b80007e789f4771f1f
GaelBy <g.benay@hotmail.fr>
2016-06-10 15:37:28 +0200

    correction index

--- index.php
+++ index.php
@@ -21,10 +21,12 @@ if (isset($_GET['page']))
 $acces_traitement = array('affichage'=>'messages','login'=>'users','logout'=>'users');
 if (array_key_exists($page, $acces_traitement))
 	require('apps/traitement_'.$acces_traitement[$page].'.php');
+	exit;
 if (isset($_GET['ajax']))
 {
 	$access_ajax = [];
 	require('apps/'.$pageAjax.'.php');
+	exit;
 }
 else
 	require('apps/skel.php');
